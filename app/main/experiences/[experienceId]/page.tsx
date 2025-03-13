fe855afb84ca043c704a4b17869ab8a65f3ba19a
alexneumayr <alex.nmr@outlook.com>
2025-03-13 01:05:50 +0100

    Add links to home page

---
+++ app/main/experiences/[experienceId]/page.tsx
@@ -0,0 +1,3 @@
+export default function IndividualExperiencePage() {
+  return <div>page</div>;
+}
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -4,6 +4,7 @@ import { getNewestExperiencesInsecure } from '@/database/experiences';
 import checkAuth from '@/util/checkAuth';
 import levelNames from '@/util/levelNames';
 import { Heart, MessageSquare } from 'lucide-react';
+import Link from 'next/link';
 import DisplayStarRating from './components/DisplayStarRating';
 
 export default async function page() {
@@ -18,68 +19,75 @@ export default async function page() {
           of our members and create your own!
         </p>
       </div>
-      <Separator className="my-4" />
+      <Separator className="mt-4" />
       {experiences.map((experience) => {
         return (
-          <div key={`experience-${experience.id}`}>
-            <div className="flex">
-              <div className="flex flex-col gap-1 items-center min-w-[150px]">
-                <Avatar className="w-[65px] h-[65px]">
-                  <AvatarImage src={`${experience.User.avatarImage}`} />
-                  <AvatarFallback>
-                    {experience.User.username.slice(0, 2).toUpperCase()}
-                  </AvatarFallback>
-                </Avatar>
-                <p className="text-sm font-bold">{experience.User.username}</p>
-                <p className="text-xs font-medium">
-                  {levelNames(experience.User.experiences.length)}
-                </p>
-                <p className="text-xs font-medium">
-                  {experience.User.experiences.length} challenges
-                </p>
-                <p className="text-xs font-medium">
-                  <span className="text-[#8d8d8d]">Gender: </span>
-                  {experience.User.gender}
-                </p>
-                <p className="text-xs font-medium text-center">
-                  <span className="text-[#8d8d8d]">Location: </span>
-                  {experience.User.location}
-                </p>
+          <Link
+            href={`/main/experiences/${experience.id}`}
+            key={`experience-${experience.id}`}
+          >
+            <div className="hover:bg-zinc-50 pt-4">
+              <div className="flex">
+                <div className="flex flex-col gap-1 items-center min-w-[150px]">
+                  <Avatar className="w-[65px] h-[65px]">
+                    <AvatarImage src={`${experience.User.avatarImage}`} />
+                    <AvatarFallback>
+                      {experience.User.username.slice(0, 2).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <p className="text-sm font-bold">
+                    {experience.User.username}
+                  </p>
+                  <p className="text-xs font-medium">
+                    {levelNames(experience.User.experiences.length)}
+                  </p>
+                  <p className="text-xs font-medium">
+                    {experience.User.experiences.length} challenges
+                  </p>
+                  <p className="text-xs font-medium">
+                    <span className="text-[#8d8d8d]">Gender: </span>
+                    {experience.User.gender}
+                  </p>
+                  <p className="text-xs font-medium text-center">
+                    <span className="text-[#8d8d8d]">Location: </span>
+                    {experience.User.location}
+                  </p>
+                </div>
+                <div className="space-y-1 p-2">
+                  <p className="text-sm font-extralight">
+                    {experience.date.toLocaleString()}
+                  </p>
+                  <h2 className="text-sm font-medium">
+                    {experience.Challenge.title}
+                  </h2>
+                  <h3 className="text-xl font-bold">{experience.title}</h3>
+                  <div>
+                    <h4 className="text-sm font-bold text-secondary">
+                      Experience:
+                    </h4>
+                    <p className="text-sm font-medium">{experience.story}</p>
+                  </div>
+                  <div className="flex items-center">
+                    <h4 className="text-sm font-bold text-secondary mr-1">
+                      Rating:
+                    </h4>
+                    <DisplayStarRating rating={experience.rating} />
+                  </div>
+                  <div className="flex gap-2 pb-2">
+                    <button className="bg-[#ededed] rounded-[100px] px-2 py-1 text-xs flex gap-1">
+                      <Heart size={15} />
+                      {experience.likes.length}
+                    </button>
+                    <button className="bg-[#ededed] rounded-[100px] px-2 py-1 text-xs flex gap-1">
+                      <MessageSquare size={15} />
+                      {experience.comments.length}
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="space-y-1 p-2">
-                <p className="text-sm font-extralight">
-                  {experience.date.toLocaleString()}
-                </p>
-                <h2 className="text-sm font-medium">
-                  {experience.Challenge.title}
-                </h2>
-                <h3 className="text-xl font-bold">{experience.title}</h3>
-                <div>
-                  <h4 className="text-sm font-bold text-secondary">
-                    Experience:
-                  </h4>
-                  <p className="text-sm font-medium">{experience.story}</p>
-                </div>
-                <div className="flex items-center">
-                  <h4 className="text-sm font-bold text-secondary mr-1">
-                    Rating:
-                  </h4>
-                  <DisplayStarRating rating={experience.rating} />
-                </div>
-                <div className="flex gap-2">
-                  <button className="bg-[#ededed] rounded-[100px] px-2 py-1 text-xs flex gap-1">
-                    <Heart size={15} />
-                    {experience.likes.length}
-                  </button>
-                  <button className="bg-[#ededed] rounded-[100px] px-2 py-1 text-xs flex gap-1">
-                    <MessageSquare size={15} />
-                    {experience.comments.length}
-                  </button>
-                </div>
-              </div>
+              <Separator className="" />
             </div>
-            <Separator className="my-4" />
-          </div>
+          </Link>
         );
       })}
     </>
