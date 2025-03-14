8d68e23b665bdef028325eed9510a173500ac1e6
alexneumayr <alex.nmr@outlook.com>
2025-03-14 14:49:27 +0100

    Fix unwanted tab switching from side nav search

--- app/main/components/SearchAreaSidebar.tsx
+++ app/main/components/SearchAreaSidebar.tsx
@@ -1,17 +1,18 @@
 'use client';
 import { Input } from '@/components/ui/input';
 import { Search } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 
 export default function SearchAreaSidebar() {
   const [searchText, setSearchText] = useState('');
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   function handleSearchFormSubmit(event: React.FormEvent) {
     event.preventDefault();
     setSearchText('');
-    const params = new URLSearchParams();
+    const params = new URLSearchParams(searchParams);
     params.set('text', searchText);
     router.push(`/main/search?${params}`);
   }
