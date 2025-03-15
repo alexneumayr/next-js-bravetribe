e9d7a659c13c09db3619faca881bb0b559d73ebb
alexneumayr <alex.nmr@outlook.com>
2025-03-15 22:32:04 +0100

    Maker header empty if no user

--- app/main/Header.tsx
+++ app/main/Header.tsx
@@ -27,76 +27,81 @@ export default async function Header() {
   const sessionTokenCookie = await getCookie('sessionToken');
   const user =
     sessionTokenCookie && (await getUserBySessionToken(sessionTokenCookie));
-  if (!user) {
-    return;
-  }
 
   return (
     <header>
       <div className="bg-secondary h-3 " />
-      <div className="px-5 flex gap-2 h-[110px] items-center justify-between  bg-white dark:bg-[#1C1C1C] shadow-[0px_4px_4px_0px_rgba(166,166,166,0.25)] w-full">
-        <p>Logo</p>
-        <div className="flex items-center gap-4">
-          <Link
-            href="/main/search"
-            className="xl:hidden hover:bg-zinc-100 rounded-[100px] p-3"
-          >
-            <Search />
-          </Link>
-          <Dialog>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Avatar className="w-[60px] h-[60px]">
-                  <AvatarImage src={`${user.avatarImage}`} />
-                  <AvatarFallback>
-                    {user.username.slice(0, 2).toUpperCase()}
-                  </AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className="cursor-pointer">
-                  Profile
-                </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer">
-                  Settings
-                </DropdownMenuItem>
-                <DialogTrigger asChild>
-                  <DropdownMenuItem className="cursor-pointer">
-                    Logout
-                  </DropdownMenuItem>
-                </DialogTrigger>
-              </DropdownMenuContent>
-            </DropdownMenu>
+      <div className="h-[110px] shadow-[0px_4px_4px_0px_rgba(166,166,166,0.25)]">
+        {user && (
+          <div className="px-5 flex gap-2 h-full items-center justify-between  bg-white dark:bg-[#1C1C1C]  w-full">
+            <p>Logo</p>
+            <div className="flex items-center gap-4">
+              <Link
+                href="/main/search"
+                className="xl:hidden hover:bg-zinc-100 rounded-[100px] p-3"
+              >
+                <Search />
+              </Link>
+              <Dialog>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Avatar className="w-[60px] h-[60px]">
+                      <AvatarImage src={`${user.avatarImage}`} />
+                      <AvatarFallback>
+                        {user.username.slice(0, 2).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="cursor-pointer">
+                      Profile
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="cursor-pointer">
+                      Settings
+                    </DropdownMenuItem>
+                    <DialogTrigger asChild>
+                      <DropdownMenuItem className="cursor-pointer">
+                        Logout
+                      </DropdownMenuItem>
+                    </DialogTrigger>
+                  </DropdownMenuContent>
+                </DropdownMenu>
 
-            <DialogContent className="max-w-[425px] space-y-4">
-              <DialogHeader>
-                <DialogTitle className="text-center font-semibold text-2xl">
-                  Do you really want to log out?
-                </DialogTitle>
-              </DialogHeader>
-              <div className="text-center text-lg text-zinc-600">
-                Log out {user.username} from BraveTribe?
-              </div>
-              <DialogFooter>
-                <form
-                  className="flex justify-around w-full gap-x-2"
-                  action={logoutUser}
-                >
-                  <Button className="w-full" type="submit">
-                    Logout
-                  </Button>
-                  <DialogClose asChild>
-                    <Button variant="outline" className="w-full" type="button">
-                      Cancel
-                    </Button>
-                  </DialogClose>
-                </form>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-        </div>
+                <DialogContent className="max-w-[425px] space-y-4">
+                  <DialogHeader>
+                    <DialogTitle className="text-center font-semibold text-2xl">
+                      Do you really want to log out?
+                    </DialogTitle>
+                  </DialogHeader>
+                  <div className="text-center text-lg text-zinc-600">
+                    Log out {user.username} from BraveTribe?
+                  </div>
+                  <DialogFooter>
+                    <form
+                      className="flex justify-around w-full gap-x-2"
+                      action={logoutUser}
+                    >
+                      <Button className="w-full" type="submit">
+                        Logout
+                      </Button>
+                      <DialogClose asChild>
+                        <Button
+                          variant="outline"
+                          className="w-full"
+                          type="button"
+                        >
+                          Cancel
+                        </Button>
+                      </DialogClose>
+                    </form>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </div>
+          </div>
+        )}
       </div>
     </header>
   );
