b0706de2c78ce847e632c65868a46ca57be43c92
alexneumayr <alex.nmr@outlook.com>
2025-03-19 00:39:58 +0100

    Add split button in planner

--- app/main/challenges/ChallengesTabs.tsx
+++ app/main/challenges/ChallengesTabs.tsx
@@ -4,17 +4,16 @@ import { Input } from '@/components/ui/input';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import type { Challenge } from '@prisma/client';
 import { Search } from 'lucide-react';
-import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import { ChallengesTableCompleted } from './ChallengesTableCompleted';
 import { ChallengesTableTodo } from './ChallengesTableTodo';
+import NewChallengeButton from './NewChallengeButton';
 
 type Props = {
   challenges: Challenge[];
 };
 
 export default function ChallengesTabs({ challenges }: Props) {
-  const router = useRouter();
   const [showSearch, setShowSearch] = useState(false);
   const [searchText, setSearchText] = useState('');
   const tabs = [
@@ -47,14 +46,9 @@ export default function ChallengesTabs({ challenges }: Props) {
         >
           <Search className="w-[18px] h-[18px]" />
         </button>
-        <Button
-          variant="secondary"
-          className="w-[52px] h-[30px]"
-          onClick={() => router.push('/main/challenges/newchallenge')}
-        >
-          New
-        </Button>
+        <NewChallengeButton />
       </div>
+
       <div
         className={`relative flex-1 transition-all ${showSearch ? 'block' : 'hidden'}`}
       >
---
+++ app/main/challenges/NewChallengeButton.tsx
@@ -0,0 +1,52 @@
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { ChevronDown, File, FileText } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+export default function NewChallengeButton() {
+  const router = useRouter();
+
+  return (
+    <div className="[&>*]:rounded-none [&>button:first-child]:rounded-l-md [&>button:last-child]:rounded-r-md divide-x divide-border/40 flex">
+      <Button
+        variant="secondary"
+        className="h-[30px] w-[50px] text-xs font-medium"
+        onClick={() => router.push('/main/challenges/newchallenge')}
+      >
+        New
+      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary" className="h-[30px] w-[20px]">
+            <ChevronDown />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="min-w-52">
+          <DropdownMenuLabel className="flex items-center justify-between gap-2">
+            New Challenge
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => router.push('/main/challenges/newchallenge')}
+          >
+            <File /> Add your own
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => router.push('/main/challenges/templates')}
+          >
+            <FileText /> Choose from template
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
